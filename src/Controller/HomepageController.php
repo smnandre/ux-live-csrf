3bd69472684d121d5fdf3cb205bb17bdf0bb5d87
Simon André <smn.andre@gmail.com>
2025-01-27 15:26:38 +0100

    fix form names

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Form\FooBarType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\FormInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -14,11 +15,7 @@ final class HomepageController extends AbstractController
     public function index(Request $request): Response
     {
         foreach (['live', 'turbo'] as $name) {
-            $form = $this->createForm(FooBarType::class, null, [
-                'action' => $this->generateUrl('app_form', ['name' => $name]),
-                 'block_name' => $name,
-            ]);
-            $forms['form_'.$name] = $form;
+            $forms['form_'.$name] = $this->createFooBarForm($name);
         }
         
         return $this->render('homepage.html.twig', [
@@ -29,26 +26,23 @@ final class HomepageController extends AbstractController
     #[Route('/form/{name}', name: 'app_form', requirements: ['name' => 'live|turbo'])]
     public function formLive(Request $request, string $name): Response
     {
-        $form = $this->createForm(FooBarType::class, null, [
-            'action' => $this->generateUrl('app_form', ['name' => $name]),
-            'block_name' => $name,
-        ]);
-        
+        $form = $this->createFooBarForm($name);
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
-            
-            if ($form->get('success')->getData()) {
-                $this->addFlash('success', 'Form submitted successfully');
-            } else {
-                $this->addFlash('error', 'Form submission failed');
-            }
-            
             return $this->redirectToRoute('app_homepage', [], Response::HTTP_SEE_OTHER);
         }
         
         return $this->render('form.html.twig', [
-            'form' => $form->createView(),
+            'form' => $form,
             'form_name' => $name,
         ]);
+    }
+    
+    private function createFooBarForm(string $name): FormInterface
+    {
+        return $this->createFormBuilder()->create($name, FooBarType::class, [
+            // 'action' => $this->generateUrl('app_form', ['name' => $name]),
+            'block_prefix' => $name,
+        ])->getForm();
     }
 }
